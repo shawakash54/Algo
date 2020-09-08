f11280e801d640edfb2b9c89a131748afd9c5dc3
Akash Shaw <shawakash54@gmail.com>
2020-09-08 22:59:34 +0530

    Create sort_k_nearly_sorted_array.java

---
+++ sort_k_nearly_sorted_array.java
@@ -0,0 +1,54 @@
+// A java program to sort a nearly sorted array 
+O(nlogk)
+import java.util.Iterator; 
+import java.util.PriorityQueue; 
+
+class GFG 
+{ 
+	private static void kSort(int[] arr, int n, int k) 
+	{ 
+	    
+	    if(k < 0 || k >arr.length)
+	        return;
+
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		
+		for(int i=0; i<k + 1; i++) {
+		    pq.add(arr[i]);
+		}
+		
+		int start = 0;
+		
+		for(int i=k + 1; i<arr.length; i++) {
+		    arr[start++] = pq.poll();
+		    pq.add(arr[i]);
+		}
+		
+		Iterator<Integer> iq = pq.iterator();
+		while(iq.hasNext()){
+		    arr[start++] = pq.poll();
+		}
+
+	} 
+
+	// A utility function to print the array 
+	private static void printArray(int[] arr, int n) 
+	{ 
+		for(int i = 0; i < n; i++) 
+			System.out.print(arr[i] + " "); 
+	} 
+
+	// Driver Code 
+	public static void main(String[] args) 
+	{ 
+		int k = 3; 
+		int arr[] = { 2, 6, 3, 12, 56, 8 }; 
+		int n = arr.length; 
+		kSort(arr, n, k); 
+		System.out.println("Following is sorted array"); 
+		printArray(arr, n); 
+	} 
+} 
+
+// This code is contributed by 
+// Manpreet Singh(manpreetsngh294) 
